bf670160bb9e2f6d639c9f330c05a4f98baba119
mumbaiops45 <mumbaiops45@gmail.com>
2026-02-21 13:47:47 +0530

    Some responsive

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+  experimental: {
+    turbo: false, 
+  },
+}
 
 export default nextConfig;
--- src/app/components/CaseStudies.jsx
+++ src/app/components/CaseStudies.jsx
@@ -1,8 +1,7 @@
 
 
 
-"use client";
-
+// "use client";
 import React from "react";
 import Link from "next/link";
 import "../styles/case.modal.css";
